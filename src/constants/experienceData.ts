feb285526a539f28e6650f13f93aed244e2c79d7
aluelmor <aluelmo@ifae.es>
2025-10-07 20:58:35 +0200

    feat: add new experience entries and logos for Deal Tax & Legal, Diari de Barcelona, and Events & Festival Host

--- src/constants/experienceData.ts
+++ src/constants/experienceData.ts
@@ -1,21 +1,31 @@
-import placeholderLogo from "@/assets/images/placeholder-logo.svg";
+import dealTaxLegalLogo from "@/assets/images/deal-tax-legal.webp";
+import diariBarcelonaLogo from "@/assets/images/diari-de-barcelona.webp";
+import eventHostessLogo from "@/assets/images/abc-personnal.webp";
 
 const experienceData = [
   {
-    id: "nexiona",
-    company: "Creative Product Studio",
-    dateRange: "2023 — 2024",
-    logo: placeholderLogo,
-    padding: "p-5",
-    website: "https://example.com",
+    id: "dealTaxLegal",
+    company: "Deal Tax & Legal",
+    dateRange: "Sep 2024 — Present",
+    logo: dealTaxLegalLogo,
+    padding: "p-0 scale-125 -translate-x-[1px] -translate-y-[1px]",
+    website: "https://dealtaxlegal.com/",
   },
   {
-    id: "fujitsu",
-    company: "Innovation Lab",
-    dateRange: "2019 — 2020",
-    logo: placeholderLogo,
-    padding: "p-5",
-    website: "https://example.com",
+    id: "diariBarcelona",
+    company: "Diari de Barcelona",
+    dateRange: "Nov 2023 — Jan 2024",
+    logo: diariBarcelonaLogo,
+    padding: "p-2.5",
+    website: "https://www.diaridebarcelona.cat/",
+  },
+  {
+    id: "eventsHostess",
+    company: "Events & Festival Host",
+    dateRange: "2022 — 2024",
+    logo: eventHostessLogo,
+    padding: "p-3",
+    website: "https://abcpersonal.es",
   },
 ];
 
