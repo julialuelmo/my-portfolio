ae80299b097b2512b16d9c075498e2f47cbc5ef6
aluelmor <aluelmo@ifae.es>
2025-10-12 23:05:03 +0200

    feat: update base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const basePath = process.env.VITE_BASE_PATH ?? "/";
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? "/my-portfolio/" : "/";
 
 export default defineConfig({
   plugins: [react()],
